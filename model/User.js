81f47fabc5b2a369a1daa2ac61383b54382190f2
Jodking <tripathihitesh580@gmail.com>
2024-10-03 19:44:12 +0530

    a

--- model/User.js
+++ model/User.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
 
+// Define the schema for the User model
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    unique: true,
+    required: true, // Name is required
+    unique: true, // Name must be unique
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: true, // Email is required
+    unique: true, // Email must be unique
   },
   password: {
     type: String,
-    required: true,
+    required: true, // Password is required
   },
   AdditionalData: {
     type: String,
-    required: false,
+    required: false, // Additional data is optional
   },
 });
 
+// Create and export the User model based on the schema
 export const User = mongoose.model("User", userSchema);
